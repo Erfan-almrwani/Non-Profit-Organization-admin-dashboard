f70a23e15d9ae95278e85f36dd94cb20a970eae5
Erfan <eng.erfan.m@gmail.com>
2025-08-10 02:20:50 +0300

    Add GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Non-Profit-Org-Site-with-tailwind-css/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
